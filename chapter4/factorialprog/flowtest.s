49da912134090370e9e6a032709a7592bfcf3d14
pherang <noreply@github.com>
2020-02-16 14:00:42 -0600

    Added some code to test the flow of a program

---
+++ chapter4/factorialprog/flowtest.s
@@ -0,0 +1,32 @@
+# Testing the flow of the program
+
+.section .data
+
+.section .text
+
+.globl _start
+.globl f1
+
+_start:
+	call f1
+
+	movl $1, %eax
+	int $0x80
+
+
+.type f1,@function
+
+f1:
+	pushl %ebp
+	movl %esp, %ebp
+	movl $5, %ebx 
+	movl %ebp, %esp
+	popl %ebp	
+	ret
+
+end_f1:
+
+	movl $6, %ebx
+	movl %ebp, %esp
+	popl %ebp	
+	ret
